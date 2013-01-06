0f23c5f4a1661291b74e9e61f9e359afa44a311b
Morten Henriksen <mh@gi2.dk>
2013-01-06 20:10:34 +0100

    Package.js added

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  summary: "Filesystem for Meteor, collectionFS"
+});
+
+Package.on_use(function(api) {
+  api.add_files('collectionFS_common.js', ['client', 'server']);
+  api.add_files('collectionFS_server.js', 'server');
+  api.add_files('collectionFSfacebook_client.js', 'client');
+});
