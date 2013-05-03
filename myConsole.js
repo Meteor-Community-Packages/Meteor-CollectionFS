d8094f3a75124b3f29fe1ab4fd3c1a8cde18611c
Morten Henriksen <mh@gi2.dk>
2013-05-03 21:34:37 +0200

    updated scope for serverConsole
    * removed scope limit
    * minor refractor
    * added supress publish warning when autopublish

--- myConsole.js
+++ myConsole.js
@@ -1,56 +1,55 @@
 /* Just a simple console to get server console in client, Regz. RaiX 2013 */
 
+// Set true to get all logs from server start
+var getAllLogs = false;
+// Enable / disable logging
+var debug = false;
+
+
 var myConsole = new Meteor.Collection('_console');
 
 serverConsole = {
 	log: function (message) {
-		console.log(message);
-		myConsole.insert({ message: message, createdAt: Date.now() });
+		if (debug) {
+			console.log(message);
+			myConsole.insert({ message: message, createdAt: Date.now() });
+		}
 	}
 };
 
-(function() {
+var timeConsole = Date.now();
 
-	var timeConsole = Date.now();
-
-	// Set true to get all logs from server start
-	var getAllLogs = false;
-	// Enable / disable logging
-	var debug = false;
-
-	if (Meteor.isClient && debug) {
-		Meteor.call('getTime', function(error, result) {
-			timeConsole = +result;
-			if (error)
-				console.log('getTime error: '+error.message);
-			console.log('Got server time: '+result);
-			Deps.autorun(function() {
-				myConsole.find({ createdAt: { $gt: timeConsole } }).forEach(function(doc) {
-					console.log('SERVER: ' + doc.message);
-					timeConsole = doc.createdAt;
-				});
+if (Meteor.isClient && debug) {
+	Meteor.call('getTime', function(error, result) {
+		timeConsole = +result;
+		if (error)
+			console.log('getTime error: '+error.message);
+		console.log('Got server time: '+result);
+		Deps.autorun(function() {
+			myConsole.find({ createdAt: { $gt: timeConsole } }).forEach(function(doc) {
+				console.log('SERVER: ' + doc.message);
+				timeConsole = doc.createdAt;
 			});
 		});
+	});
 
-		if (debug)
-			Meteor.subscribe('myConsole');
-	}
+	if (debug)
+		Meteor.subscribe('myConsole');
+}
 
-	if (Meteor.isServer && debug) {
+if (Meteor.isServer && debug) {
 
-		myConsole.remove({});
+	myConsole.remove({});
 
-		if (debug)
-			Meteor.publish('myConsole', function() {
-				return myConsole.find({ createdAt : { $gt: timeConsole } });
-			});
+	if (debug)
+		Meteor.publish('myConsole', function() {
+			return myConsole.find({ createdAt : { $gt: timeConsole } });
+		}, {is_auto: true});
 
-		Meteor.methods({
-			getTime: function() {
-				serverConsole.log('getTime');
-				return (getAllLogs)? 0 : Date.now()-20000; // Just add a little slack
-			}
-		});
-	}
-
-})();
+	Meteor.methods({
+		getTime: function() {
+			serverConsole.log('getTime');
+			return (getAllLogs)? 0 : Date.now()-20000; // Just add a little slack
+		}
+	});
+}
