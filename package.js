6ddd8c2d213c02c9d1f33aba506e2de523b94d28
Morten Henriksen <mh@gi2.dk>
2013-09-11 10:27:58 +0200

    Updated deps

--- package.js
+++ package.js
@@ -1,9 +1,16 @@
 Package.describe({
-  summary: "Filesystem for Meteor, collectionFS"
+    summary: "\u001b[32mv0.3.0 (under development)\n"+
+         "\u001b[33m-----------------------------------------\n"+
+         "\u001b[0m Filesystem for Meteor, collectionFS      \n"+
+         "\u001b[0m                                          \n"+
+         "\u001b[33m-------------------------------------RaiX\n"
 });
 
 Package.on_use(function(api) {
-  api.use(['deps', 'underscore', 'templating']);
+  "use strict";
+  api.use(['deps', 'underscore', 'templating', 'handlebars', 'mongo-livedata']);
+
+  api.export && api.export('CollectionFS');
 
   api.add_files([ 'myConsole.js' ], [ 'client', 'server' ]);
 
@@ -13,13 +20,13 @@ Package.on_use(function(api) {
           'collectionFS_client.js',
           'collectionFS_client.api.js',
           'collectionFS_handlebars.js'], 'client');
-  
+
   api.add_files([
   				'collectionFS_filesystem.js',
   				'collectionFS_server.js',
   				'collectionFS_filehandlers.js',
           'collectionFS_server.api.js'], 'server');
-  
+
   api.add_files([
           'collectionFS_common.js',
           'numeral.js'], ['client', 'server']);
