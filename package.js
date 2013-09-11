fb844c2b350d9eb4f60c6ff35e2a056ff1e006d5
Morten Henriksen <mh@gi2.dk>
2013-09-11 12:29:27 +0200

    jshint clean up

--- package.js
+++ package.js
@@ -1,34 +1,34 @@
 Package.describe({
-    summary: "\u001b[32mv0.3.0 (under development)\n"+
-         "\u001b[33m-----------------------------------------\n"+
-         "\u001b[0m Filesystem for Meteor, collectionFS      \n"+
-         "\u001b[0m                                          \n"+
-         "\u001b[33m-------------------------------------RaiX\n"
+    summary: '\u001b[32mv0.3.0 (under development)\n'+
+         '\u001b[33m-----------------------------------------\n'+
+         '\u001b[0m Filesystem for Meteor, collectionFS      \n'+
+         '\u001b[0m                                          \n'+
+         '\u001b[33m-------------------------------------RaiX\n'
 });
 
 Package.on_use(function(api) {
-  "use strict";
+  'use strict';
   api.use(['deps', 'underscore', 'templating', 'handlebars', 'mongo-livedata']);
 
-  api.export && api.export('CollectionFS');
+  api.export && api.export(['CollectionFS', 'CFSErrorType']);
 
   api.add_files([ 'myConsole.js' ], [ 'client', 'server' ]);
 
   api.add_files([
-            'FileSaver.js',
-          'collectionFS_templates.html',
-          'collectionFS_client.js',
-          'collectionFS_client.api.js',
-          'collectionFS_handlebars.js'], 'client');
+    'FileSaver.js',
+    'collectionFS_templates.html',
+    'collectionFS_client.js',
+    'collectionFS_client.api.js',
+    'collectionFS_handlebars.js'], 'client');
 
   api.add_files([
-  				'collectionFS_filesystem.js',
-  				'collectionFS_server.js',
-  				'collectionFS_filehandlers.js',
-          'collectionFS_server.api.js'], 'server');
+    'collectionFS_filesystem.js',
+    'collectionFS_server.js',
+    'collectionFS_filehandlers.js',
+    'collectionFS_server.api.js'], 'server');
 
   api.add_files([
-          'collectionFS_common.js',
-          'numeral.js'], ['client', 'server']);
+    'collectionFS_common.js',
+    'numeral.js'], ['client', 'server']);
 
 });
