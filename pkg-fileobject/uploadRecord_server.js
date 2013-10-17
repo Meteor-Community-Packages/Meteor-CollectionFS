be8ba92363d34b04c8919e8b96bc45aa4b6bab7c
Eric Dobbertin <eric@dairystatedesigns.com>
2013-10-17 18:04:56 -0500

    better failure handling for removeCopy

--- pkg-fileobject/uploadRecord_server.js
+++ pkg-fileobject/uploadRecord_server.js
@@ -2,14 +2,20 @@ UploadRecord.prototype.removeCopy = function(fileHandler) {
   var self = this, funcs = self._uploadsCollection._fileHandlers[fileHandler];
   var fhValue = self.fileHandler[fileHandler];
   if (funcs && funcs.del && fhValue) {
-    var success = funcs.del.call(self.toFileObject(), fhValue);
-    if (!success)
-      throw new Error("The del function for the " + fileHandler + " filehandler failed for file with ID " + self._id);
+    var success;
+    try {
+      success = funcs.del.call(self.toFileObject(), fhValue);
+    } catch (e) {
+      success = false;
+    }
 
     //set filehandler value to null to indicate that no file exists for this handler
     var setObj = {};
     setObj["fileHandler." + fileHandler] = null;
     self._uploadsCollection._collection.update({_id: self._id}, {$set: setObj});
+    
+    if (!success)
+      throw new Error("The del function for the " + fileHandler + " filehandler failed for file with ID " + self._id);
   }
 };
 
