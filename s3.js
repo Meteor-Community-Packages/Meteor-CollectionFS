3eb12d64055f89188c1d03ae3a4c5fb98e1adf4b
Eric Dobbertin <eric@dairystatedesigns.com>
2014-01-09 01:29:08 -0600

    fix stuff that's broken

--- s3.js
+++ s3.js
@@ -9,39 +9,68 @@ FS.S3Store = function(name, options) {
     style: "path",
     'x-amz-acl': 'public-read'
   }, options);
+  
+  // Determine which folder (key prefix) in the bucket to use
+  var folder = options.folder;
+  if (typeof folder === "string" && folder.length) {
+    if (folder.slice(0, 1) !== "/") {
+      folder = "/" + folder;
+    }
+    if (folder.slice(-1) !== "/") {
+      folder += "/";
+    }
+  } else {
+    folder = "/";
+  }
 
   var S3 = Knox.createClient(options);
 
   return new FS.StorageAdapter(name, {}, {
     typeName: 'storage.s3',
     get: function(fileKey, callback) {
+      var hasReturned = false; // prevent calling the callback more than once
+      var bufs = []; 
       var req = S3.get(fileKey);
       req.on('response', function(res) {
         res.on('data', function(chunk) {
-          callback(null, chunk);
+          bufs.push(chunk);
+        });
+        res.on('end', function() {
+          var buffer = Buffer.concat(bufs);
+          !hasReturned && callback(null, buffer);
+          hasReturned = true;
         });
       });
       req.on('error', function(e) {
-        callback(e);
+        !hasReturned && callback(e);
+        hasReturned = true;
       });
       req.end();
     },
     put: function(id, fileKey, buffer, opts, callback) {
       opts = opts || {};
+      var hasReturned = false; // prevent calling the callback more than once
       var headers = {
         'Content-Length': buffer.length,
         'Content-Type': opts.type,
         'x-amz-acl': options['x-amz-acl']
       };
+      fileKey = folder + id + "/" + fileKey;
       var req = S3.putBuffer(buffer, fileKey, headers, function(err, res) {
-        if (res && req && res.statusCode === 200 && req.url) {
-          callback(null, fileKey);
+        if (res && res.statusCode === 200 && req && req.url) {
+          !hasReturned && callback(null, fileKey);
+        } else if (res && res.statusCode) {
+          !hasReturned && callback(new Error("S3 Storage Error: S3 returned status code " + res.statusCode));
+        } else if (err) {
+          !hasReturned && callback(err);
         } else {
-          callback(new Error("S3 putBuffer failed"));
+          !hasReturned && callback(new Error("Unknown S3 Storage Error"));
         }
+        hasReturned = true;
       });
-      req.on('error', function(e) {
-        callback(e);
+      req.on('error', function(err) {
+        !hasReturned && callback(err);
+        hasReturned = true;
       });
     },
     del: function(fileKey, callback) {
