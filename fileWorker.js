ab4d0c332f5779e00cac1ce559304ef7084fe82c
Eric Dobbertin <eric@dairystatedesigns.com>
2014-02-04 12:51:57 -0600

    refactor to expose saveCopy to API; then change fileworker observes to be per-copy, calling saveCopy and allowing better control of which copies to create at which times

--- fileWorker.js
+++ fileWorker.js
@@ -12,23 +12,24 @@ FileWorker = {};
  * @returns {undefined}
  */
 FileWorker.observe = function(fsCollection) {
-  
+
   // Initiate observe for finding newly uploaded/added files that need to be stored
-  fsCollection.files.find(getReadyQuery(fsCollection.options.copies)).observe({
-    added: function(fsFile) {
-      FS.debug && console.log("FileWorker ADDED - calling saveCopies for", fsFile._id);
-      fsCollection.saveCopies(fsFile);
-    },
-    changed: function(fsFile) {
-      FS.debug && console.log("FileWorker CHANGED - calling saveCopies for", fsFile._id);
-      // Might be in the process of storing copies, and we were notified
-      // because info for one copy was added. We can call saveCopies anyway
-      // and it should be OK, but there might be race conditions. TODO
-      // make this more foolproof.
-      fsCollection.saveCopies(fsFile);
-    }
+  // per store.
+  _.each(fsCollection.options.copies, function(copyDefinition, copyName) {
+    fsCollection.files.find(getReadyQuery(copyName)).observe({
+      added: function(fsFile) {
+        // added will catch fresh files
+        FS.debug && console.log("FileWorker ADDED - calling saveCopy", copyName, "for", fsFile._id);
+        fsCollection.saveCopy(fsFile, copyName);
+      },
+      changed: function(fsFile) {
+        // changed will catch failures and retry them
+        FS.debug && console.log("FileWorker CHANGED - calling saveCopy", copyName, "for", fsFile._id);
+        fsCollection.saveCopy(fsFile, copyName);
+      }
+    });
   });
-  
+
   // Initiate observe for finding files that have been stored so we can delete
   // any temp files
   fsCollection.files.find(getDoneQuery(fsCollection.options.copies)).observe({
@@ -37,7 +38,7 @@ FileWorker.observe = function(fsCollection) {
       TempStore.deleteChunks(fsFile);
     }
   });
-  
+
   // Initiate observe for catching files that have been removed and
   // removing the data from all stores as well
   fsCollection.files.find().observe({
@@ -53,34 +54,26 @@ FileWorker.observe = function(fsCollection) {
 
 /**
  *  Returns a selector that will be used to identify files that
- *  have been uploaded but have not yet been stored to one or
- *  more stores.
+ *  have been uploaded but have not yet been stored to the
+ *  specified store.
  *  
  *  {
  *    $where: "this.bytesUploaded === this.size",
- *    $or: [
- *      {
- *        'copies.copy1`: null,
- *        'failures.copies.copy1.doneTrying': {$ne: true}
- *      },
- *      REPEATED FOR EACH COPY
- *    ]
+ *    'copies.copyName`: null,
+ *    'failures.copies.copyName.doneTrying': {$ne: true}
  *  }
+ *  
+ *  @param {string} copyName - The name of the store to observe
  */
-var getReadyQuery = function getReadyQuery(copies) {
+var getReadyQuery = function getReadyQuery(copyName) {
   var selector = {
     $where: "this.bytesUploaded === this.size",
-    $or: []
+    chunks: {$exists: true}
   };
-  
-  // Add conditions for all defined copies
-  for (var copyName in copies) {
-    var test = {};
-    test['copies.' + copyName] = null;
-    test['failures.copies.' + copyName + '.doneTrying'] = {$ne: true};
-    selector.$or.push(test);
-  }
-  
+
+  selector['copies.' + copyName] = null;
+  selector['failures.copies.' + copyName + '.doneTrying'] = {$ne: true};
+
   return selector;
 };
 
@@ -110,9 +103,11 @@ var getReadyQuery = function getReadyQuery(copies) {
  *          }
  *        ]
  *      },
+ *      REPEATED FOR EACH COPY
  *    ]
- *    REPEATED FOR EACH COPY
  *  }
+ *  
+ *  @param {Object} copies - The copies object from the FS.Collection options
  */
 var getDoneQuery = function getDoneQuery(copies) {
   var selector = {
@@ -120,7 +115,7 @@ var getDoneQuery = function getDoneQuery(copies) {
       {chunks: {$exists: true}}
     ]
   };
-  
+
   // Add conditions for all defined copies
   for (var copyName in copies) {
     var copyCond = {$or: [{$and: []}]};
@@ -135,6 +130,6 @@ var getDoneQuery = function getDoneQuery(copies) {
     copyCond.$or.push(tempCond);
     selector.$and.push(copyCond);
   }
-  
+
   return selector;
 };
--- fsCollection/api.server.js
+++ fsCollection/api.server.js
@@ -11,7 +11,7 @@ function loadBuffer(fsFile, callback) {
   // If the supplied fsFile does not have a buffer loaded already,
   // try to load it from the temporary file.
   FS.debug && console.log("TempStore: attempting to load buffer from temp file");
-  TempStore.getDataForFile(fsFile, function (err, fsFileWithData) {
+  TempStore.getDataForFile(fsFile, function(err, fsFileWithData) {
     if (err) {
       callback(err);
     } else {
@@ -24,36 +24,72 @@ function loadBuffer(fsFile, callback) {
 
 var loadBufferSync = Meteor._wrapAsync(loadBuffer);
 
-function saveCopy(fsFile, store, beforeSave) {
-  var fsFileClone;
-          
-  // Get a new copy and a fresh buffer each time in case beforeSave changes anything
-  try {
-    fsFileClone = loadBufferSync(fsFile);
-  } catch (err) {
-    return false;
-  }
+/**
+ * Saves to the specified store. If the
+ * `overwrite` option is `true`, will save to the store even if we already
+ * have, potentially overwriting any previously saved data. Synchronous.
+ * 
+ * @param {FS.File} fsFile
+ * @param {string} copyName
+ * @param {Object} options
+ * @param {Boolean} [options.overwrite=false] - Force save to the specified store?
+ * @returns {undefined}
+ */
+FS.Collection.prototype.saveCopy = function(fsFile, copyName, options) {
+  var self = this;
+  options = options || {};
+  var copyInfo = fsFile.copies && fsFile.copies[copyName];
+  var copyDefinition = self.options.copies[copyName];
 
-  // Call the beforeSave function provided by the user
-  if (!beforeSave ||
-          beforeSave.apply(fsFileClone) !== false) {
-    var id = store.insert(fsFileClone);
-    if (!id) {
-      return null;
-    } else {
-      return {
-        _id: id,
-        name: fsFileClone.name,
-        type: fsFileClone.type,
-        size: fsFileClone.size,
-        utime: fsFileClone.utime
-      };
+  // If copy has not already been saved or we want to overwrite it
+  if (options.overwrite || (copyInfo === void 0 && !fsFile.failedPermanently(copyName))) {
+    FS.debug && console.log('creating copy ' + copyName);
+
+    var fsFileClone, result;
+
+    // Get a new copy and a fresh buffer each time in case beforeSave changes anything
+    try {
+      fsFileClone = loadBufferSync(fsFile);
+    } catch (err) {
+      result = false;
     }
-  } else if (beforeSave) {
-    //beforeSave returned false
-    return false;
+
+    // Call the beforeSave function provided by the user
+    if (!copyDefinition.beforeSave ||
+            copyDefinition.beforeSave.apply(fsFileClone) !== false) {
+      var id = copyDefinition.store.insert(fsFileClone);
+      if (!id) {
+        result = null;
+      } else {
+        result = {
+          _id: id,
+          name: fsFileClone.name,
+          type: fsFileClone.type,
+          size: fsFileClone.size,
+          utime: fsFileClone.utime
+        };
+      }
+    } else if (copyDefinition.beforeSave) {
+      //beforeSave returned false
+      result = false;
+    }
+
+    if (result === null) {
+      // Temporary failure; let the fsFile log it and potentially decide
+      // to give up.
+      fsFile.logCopyFailure(copyName, copyDefinition.maxTries);
+    } else {
+      // Update the main file object
+      // copyInfo might be false, which indicates that this copy
+      // should never be created in the future.
+      var modifier = {};
+      modifier["copies." + copyName] = result;
+      // Update the main file object with the modifier
+      fsFile.update({$set: modifier});
+    }
   }
-}
+  
+};
 
 /**
  * Saves to any stores that data has not yet been saved to. If the
@@ -67,30 +103,10 @@ function saveCopy(fsFile, store, beforeSave) {
  */
 FS.Collection.prototype.saveCopies = function(fsFile, options) {
   var self = this;
-  options = options || {};
-
+  
   // Loop through copies defined in CFS options
   _.each(self.options.copies, function(copyDefinition, copyName) {
-    var copyInfo = fsFile.copies && fsFile.copies[copyName];
-    // If copy has not already been saved or we want to overwrite it
-    if (options.overwrite || (copyInfo === void 0 && !fsFile.failedPermanently(copyName))) {
-      FS.debug && console.log('creating copy ' + copyName);
-
-      var result = saveCopy(fsFile, copyDefinition.store, copyDefinition.beforeSave);
-      if (result === null) {
-        // Temporary failure; let the fsFile log it and potentially decide
-        // to give up.
-        fsFile.logCopyFailure(copyName, copyDefinition.maxTries);
-      } else {
-        // Update the main file object
-        // copyInfo might be false, which indicates that this copy
-        // should never be created in the future.
-        var modifier = {};
-        modifier["copies." + copyName] = result;
-        // Update the main file object with the modifier
-        fsFile.update({$set: modifier});
-      }
-    }
+    self.saveCopy(fsFile, copyName, options);
   });
 };
 
