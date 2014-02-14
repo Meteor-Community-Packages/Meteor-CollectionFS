4077dd4ce6e77a8435d0f75025d887a5ea768141
Eric Dobbertin <eric@dairystatedesigns.com>
2014-02-14 09:37:05 -0600

    fix typo

--- package.js
+++ package.js
@@ -23,5 +23,5 @@ Package.on_test(function (api) {
   api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
            'random', 'deps']);
 
-  api.add_files('tests/client-tests.js', 'server');
+  api.add_files('tests/server-tests.js', 'server');
 });
