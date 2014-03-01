428eb68f0b7dcc7cd397141297d133e746a7a153
Eric Dobbertin <eric@dairystatedesigns.com>
2014-03-01 16:46:55 -0600

    just package and doc tweaks

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-CollectionFS
+cfs-worker
 =========================
+
+This is a Meteor package used by
+[CollectionFS](https://github.com/CollectionFS/Meteor-CollectionFS).
+
+You don't need to manually add this package to your app. It is added when you
+add the `collectionFS` package.
--- package.js
+++ package.js
@@ -1,19 +1,12 @@
 Package.describe({
+  name: 'cfs-worker',
   summary: 'CollectionFS, file worker - handles file copies/versions'
 });
 
-// Npm.depends({
-//   mime: "1.2.11",
-//   temp: "0.6.0"
-// });
-
 Package.on_use(function(api) {
-
-
   api.use(['cfs-base-package', 'cfs-tempstore']);
 
-  api.use(['deps', 'underscore', 'check', 'livedata', 'mongo-livedata',
-    'ejson', 'power-queue']);
+  api.use(['underscore', 'livedata', 'mongo-livedata', 'power-queue']);
 
   api.add_files([
     'fileWorker.js'
@@ -21,12 +14,11 @@ Package.on_use(function(api) {
 });
 
 Package.on_test(function (api) {
-  api.use('cfs-worker');
+  api.use('collectionFS');
 
   api.use('test-helpers', 'server');
-  api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
-           'random', 'deps']);
+  api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict', 'random']);
 
-  api.add_files('tests/client-tests.js', 'server');
-  api.add_files('tests/server-tests.js', 'client');
+  api.add_files('tests/client-tests.js', 'client');
+  api.add_files('tests/server-tests.js', 'server');
 });
