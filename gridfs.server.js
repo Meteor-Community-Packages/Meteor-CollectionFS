a23a3eead08fa6ec24739882bdef2c21b76f2711
Vaughn Iverson <vsi@Vaughns-Mac-mini-2.local>
2014-03-02 13:17:32 -0800

    Meteor_wrap_async calls removed.  Untested.

--- gridfs.server.js
+++ gridfs.server.js
@@ -34,13 +34,28 @@ FS.Store.GridFS = function(name, options) {
       var fileInfo = fileObj.getCopyInfo(name);
       if (!fileInfo) { return callback(null, null); }
       var fileKey = fileInfo.key;
-      var existing = Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {});
-      if (!existing) { return callback(null, null); }
-      var gs = new mongodb.GridStore(self.db, fileKey, 'r', { root: name });
-      gs = Meteor._wrapAsync(gs.open.bind(gs))();
-      var result = Meteor._wrapAsync(gs.read.bind(gs))();
-      Meteor._wrapAsync(gs.close.bind(gs))();
-      callback(null, new Uint8Array(result));
+      mongodb.GridStore.exist(self.db, fileKey, name, {}, function (err, existing) {
+        if (err) { return callback(err); }
+        if (!existing) { return callback(null, null); }
+        var gstore = new mongodb.GridStore(self.db, fileKey, 'r', { root: name });
+        gstore.open(function (err, gs) {
+          if (err) { return callback(err); }
+          gs.read(function (err, result) {
+            if (err) { return callback(err); }
+            gs.close(function (err) {
+              if (err) { return callback(err); }
+              callback(null, new Uint8Array(result));
+            });
+          });
+        });
+      });
+      // var existing = Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {});
+      // if (!existing) { return callback(null, null); }
+      // var gs = new mongodb.GridStore(self.db, fileKey, 'r', { root: name });
+      // gs = Meteor._wrapAsync(gs.open.bind(gs))();
+      // var result = Meteor._wrapAsync(gs.read.bind(gs))();
+      // Meteor._wrapAsync(gs.close.bind(gs))();
+      // callback(null, new Uint8Array(result));
     },
 
     getBytes: function(fileObj, start, end, callback) {
@@ -48,46 +63,96 @@ FS.Store.GridFS = function(name, options) {
       var fileInfo = fileObj.getCopyInfo(name);
       if (!fileInfo) { return callback(null, null); }
       var fileKey = fileInfo.key;
-      var existing = Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {});
-      if (!existing) { return callback(null, null); }
-      var gs = new mongodb.GridStore(self.db, fileKey, 'r', { root: name });
-      gs = Meteor._wrapAsync(gs.open.bind(gs))();
-      Meteor._wrapAsync(gs.seek.bind(gs))(start);
-      var result = Meteor._wrapAsync(gs.read.bind(gs))(end - start);
-      Meteor._wrapAsync(gs.close.bind(gs))();
-      callback(null, new Uint8Array(result));
+      mongodb.GridStore.exist(self.db, fileKey, name, {}, function (err, existing) {
+        if (err) { return callback(err); }
+        if (!existing) { return callback(null, null); }
+        var gstore = new mongodb.GridStore(self.db, fileKey, 'r', { root: name });
+        gstore.open(function (err, gs) {
+          if (err) { return callback(err); }
+          gs.seek(start, function (err) {
+            if (err) { return callback(err); }
+            gs.read(end - start, function (err, result) {
+              if (err) { return callback(err); }
+              gs.close(function (err) {
+                if (err) { return callback(err); }
+                callback(null, new Uint8Array(result));
+              });
+            });
+          });
+        });
+      });
+      // var existing = Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {});
+      // if (!existing) { return callback(null, null); }
+      // var gs = new mongodb.GridStore(self.db, fileKey, 'r', { root: name });
+      // gs = Meteor._wrapAsync(gs.open.bind(gs))();
+      // Meteor._wrapAsync(gs.seek.bind(gs))(start);
+      // var result = Meteor._wrapAsync(gs.read.bind(gs))(end - start);
+      // Meteor._wrapAsync(gs.close.bind(gs))();
+      // callback(null, new Uint8Array(result));
     },
 
     put: function(fileObj, options, callback) {
       var self = this;
       options = options || {};
-      
+
       var fileKey = fileObj.collectionName + fileObj._id;
       var buffer = fileObj.getBuffer();
-      var existing = Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {});
 
-      if (existing && !options.overwrite) {
-        // Alter the recommended fileKey until we have one that is unique
-        var fn = fileKey;
-        var suffix = 0;
-        do {
-          suffix++;
-          fileKey = fn + '_' + suffix; //once we exit the loop, this is what will actually be used
-        } while (Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {}));
-      }
-
-      var gridOptions = {
-        root: name,
-        chunk_size: options.chunk_size || chunkSize,
-        metadata: fileObj.metadata || null,
-        content_type: fileObj.type || 'application/octet-stream'
+      // Write buffer to store once we have a suitable fileKey
+      var writeBuffer = function (goodFileKey) {
+        var gridOptions = {
+          root: name,
+          chunk_size: options.chunk_size || chunkSize,
+          metadata: fileObj.metadata || null,
+          content_type: fileObj.type || 'application/octet-stream'
+        };
+        var gstore = new mongodb.GridStore(self.db, goodFileKey, 'w', gridOptions);
+        gstore.open(function (err, gs) {
+          if (err) { return callback(err); }
+          gs.write(buffer, function (err, result) {
+            if (err) { return callback(err); }
+            gs.close(function (err) {
+              if (err) { return callback(err); }
+              callback(null, goodFileKey);
+            });
+          });
+        });
       };
 
-      var gs = new mongodb.GridStore(self.db, fileKey, 'w', gridOptions);
-      gs = Meteor._wrapAsync(gs.open.bind(gs))();
-      var result = Meteor._wrapAsync(gs.write.bind(gs))(buffer);
-      Meteor._wrapAsync(gs.close.bind(gs))();
-      callback(null, fileKey);
+      if (options.overwrite) {
+        writeBuffer(fileKey);
+      } else {
+        var fn = fileKey;
+        var suffix = 0;
+        // XXX: Icky recursive async call to find non-existing fileKey
+        var findGoodFileKey = function (err, existing) {
+          if (err) { return callback(err); }
+          if (existing) {
+            suffix++;
+            fileKey = fn + '_' + suffix;
+            mongodb.GridStore.exist(self.db, fileKey, name, {}, findGoodFileKey);
+          } else {
+            writeBuffer(fileKey);
+          }
+        };
+        mongodb.GridStore.exist(self.db, fileKey, name, {}, findGoodFileKey);
+      }
+
+      // var existing = Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {});
+      // if (existing && !options.overwrite) {
+      //   // Alter the recommended fileKey until we have one that is unique
+      //   var fn = fileKey;
+      //   var suffix = 0;
+      //   do {
+      //     suffix++;
+      //     fileKey = fn + '_' + suffix; //once we exit the loop, this is what will actually be used
+      //   } while (Meteor._wrapAsync(mongodb.GridStore.exist)(self.db, fileKey, name, {}));
+      // }
+      // var gs = new mongodb.GridStore(self.db, fileKey, 'w', gridOptions);
+      // gs = Meteor._wrapAsync(gs.open.bind(gs))();
+      // var result = Meteor._wrapAsync(gs.write.bind(gs))(buffer);
+      // Meteor._wrapAsync(gs.close.bind(gs))();
+      // callback(null, fileKey);
     },
 
     del: function(fileObj, callback) {
@@ -95,17 +160,29 @@ FS.Store.GridFS = function(name, options) {
       var fileInfo = fileObj.getCopyInfo(name);
       if (!fileInfo) { return callback(null, true); }
       var fileKey = fileInfo.key;
-      Meteor._wrapAsync(mongodb.GridStore.unlink)(self.db, fileKey, { root: name });
-      callback(null, true);
+      mongodb.GridStore.unlink(self.db, fileKey, { root: name }, function (err) {
+        if (err) { return callback(err); }
+        callback(null, true);
+      });
+      // Meteor._wrapAsync(mongodb.GridStore.unlink)(self.db, fileKey, { root: name });
+      // callback(null, true);
     },
 
     watch: function() {
       throw new Error("GridFS storage adapter does not support the sync option");
     },
 
-    init: function() {
+    init: function(callback) {
       var self = this;
-      self.db = Meteor._wrapAsync(mongodb.MongoClient.connect)(options.mongoUrl);
+      if (callback) {
+        mongodb.MongoClient.connect(options.mongoUrl, function (err, db) {
+          if (err) { return callback(err); }
+          self.db = db;
+          callback(null);
+        });
+      } else {  // XXX: remove once callback is implemented
+        self.db = Meteor._wrapAsync(mongodb.MongoClient.connect)(options.mongoUrl);
+      }
     }
   });
 };
