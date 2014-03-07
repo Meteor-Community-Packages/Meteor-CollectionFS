5047505d68d154ef529fa55f21bb44bc15968976
Eric Dobbertin <eric@dairystatedesigns.com>
2014-03-07 16:31:30 -0600

    rename temp store collection to 'cfs.tempstore'

--- tempStore.js
+++ tempStore.js
@@ -1,42 +1,44 @@
-// #Temporary Storage
-// Temporary storage is used for chunked uploads until all chunks are received
-// and all copies have been made or given up. In some cases, the original file
-// is stored only in temporary storage (for example, if all copies do some
-// manipulation in beforeSave). This is why we use the temporary file as the
-// basis for each saved copy, and then remove it after all copies are saved.
-//
-// Every chunk is saved as an individual temporary file. This is safer than
-// attempting to write multiple incoming chunks to different positions in a
-// single temporary file, which can lead to write conflicts.
-//
-// Using temp files also allows us to easily resume uploads, even if the server
-// restarts, and to keep the working memory clear.
+/*
+ * Temporary Storage
+ * 
+ * Temporary storage is used for chunked uploads until all chunks are received
+ * and all copies have been made or given up. In some cases, the original file
+ * is stored only in temporary storage (for example, if all copies do some
+ * manipulation in beforeSave). This is why we use the temporary file as the
+ * basis for each saved copy, and then remove it after all copies are saved.
+ * 
+ * Every chunk is saved as an individual temporary file. This is safer than
+ * attempting to write multiple incoming chunks to different positions in a
+ * single temporary file, which can lead to write conflicts.
+ * 
+ * Using temp files also allows us to easily resume uploads, even if the server
+ * restarts, and to keep the working memory clear.
+ */
 
 fs = Npm.require('fs');
 tmp = Npm.require('temp');
 
-var storeCollection = new Meteor.Collection("fs.storage.temp");
+var storeCollection = new Meteor.Collection("cfs.tempstore");
 
 /** @namespace FS.TempStore
-  * @property FS.TempStore
-  * @type {object}
-  */
+ * @property FS.TempStore
+ * @type {object}
+ */
 FS.TempStore = {
-
   /** @method FS.TempStore.saveChunk
-    * @param {FS.File} fsFile
-    * @param {Buffer} buffer
-    * @param {number} start
-    * @param {function} callback callback(err)
-    * @todo In some ways it would make sense to save chunks into temp folder pr. file, naming the chunks `1.bin`, `2.bin` ... `n.bin`
-    */
-  saveChunk: function(fileObj, buffer, start, callback) {
+   * @param {FS.File} fsFile
+   * @param {Buffer} buffer
+   * @param {number} start
+   * @param {function} callback callback(err)
+   * @todo In some ways it would make sense to save chunks into temp folder pr. file, naming the chunks `1.bin`, `2.bin` ... `n.bin`
+   */
+  saveChunk: function saveChunk(fileObj, buffer, start, callback) {
     var total = buffer.length, tempFile, bytesChange;
 
     if (typeof callback !== "function") {
       throw new Error("FS.File.saveChunk requires a callback");
     }
-    
+
     var existing = storeCollection.findOne({fileId: fileObj._id, collectionName: fileObj.collectionName, start: start});
     if (existing) {
       tempFile = existing.tempFile;
@@ -55,7 +57,7 @@ FS.TempStore = {
       });
       bytesChange = total;
     }
-    
+
     // Write the chunk data into the temporary file
     fs.writeFile(tempFile, buffer, Meteor.bindEnvironment(function(err) {
       if (err) {
@@ -69,25 +71,24 @@ FS.TempStore = {
       callback(err);
     }));
   },
-
   /** @method FS.TempStore.getDataForFile
-    * @param {FS.File} fileObj
-    * @param {function} callback callback(err, fileObjWithData)
-    * @todo This cannot handle large files eg. 2gb or more?
-    */
-  getDataForFile: function(fileObj, callback) {
-    
+   * @param {FS.File} fileObj
+   * @param {function} callback callback(err, fileObjWithData)
+   * @todo This cannot handle large files eg. 2gb or more?
+   */
+  getDataForFile: function getDataForFile(fileObj, callback) {
+
     var existing = storeCollection.find({fileId: fileObj._id, collectionName: fileObj.collectionName});
     if (!existing.count()) {
       callback(new Error('getDataForFile: No temporary chunks!'));
       return;
     }
-    
+
     existing.rewind();
-    
+
     var tempBinary = EJSON.newBinary(fileObj.size);
     var total = 0, stop = false;
-    existing.forEach(function (chunk) {
+    existing.forEach(function(chunk) {
       if (!stop) {
         var start = chunk.start;
         // Call node readFile
@@ -116,15 +117,14 @@ FS.TempStore = {
       }
     });
   },
-
   /** @method FS.TempStore.deleteChunks
-    * @param {FS.File} fileObj
-    * @param {function} callback callback(err)
-    */
-  deleteChunks: function(fileObj, callback) {
+   * @param {FS.File} fileObj
+   * @param {function} callback callback(err)
+   */
+  deleteChunks: function deleteChunks(fileObj, callback) {
     var stop = false, count, deletedCount = 0;
     callback = callback || FS.Utility.defaultCallback;
-    
+
     var existing = storeCollection.find({fileId: fileObj._id, collectionName: fileObj.collectionName});
 
     if (!existing.count()) {
@@ -139,10 +139,10 @@ FS.TempStore = {
         callback();
       }
     }
-    
+
     existing.rewind();
-    
-    existing.forEach(function (chunk) {
+
+    existing.forEach(function(chunk) {
       if (!stop) {
         if (!fs.existsSync(chunk.tempFile)) {
           success();
@@ -162,21 +162,20 @@ FS.TempStore = {
       }
     });
   },
-
   /** @method FS.TempStore.ensureForFile
-    * @param {FS.File} fileObj
-    * @param {function} callback callback(err)
-    */
-  ensureForFile: function (fileObj, callback) {
+   * @param {FS.File} fileObj
+   * @param {function} callback callback(err)
+   */
+  ensureForFile: function ensureForFile(fileObj, callback) {
     callback = callback || FS.Utility.defaultCallback;
     FS.TempStore.saveChunk(fileObj, fileObj.getBuffer(), 0, callback);
   }
 };
 
 /** @method FS.TempStore.getDataForFileSync
-  * @param {FS.File} fileObj
-  * 
-  * Synchronous version of FS.TempStore.getDataForFile. Returns the file
-  * with data attached, or throws an error.
-  */
+ * @param {FS.File} fileObj
+ * 
+ * Synchronous version of FS.TempStore.getDataForFile. Returns the file
+ * with data attached, or throws an error.
+ */
 FS.TempStore.getDataForFileSync = Meteor._wrapAsync(FS.TempStore.getDataForFile);
