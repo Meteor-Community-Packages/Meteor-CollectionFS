670561743574ae8bf658bbb8634e5b9ebb8ad303
Morten Henriksen <mh@gi2.dk>
2014-03-09 16:02:28 +0100

    Add unmount and allow mount to use default selector function

--- access-point-server.js
+++ access-point-server.js
@@ -59,26 +59,66 @@ FS.HTTP.unpublish = function fsHttpUnpublish(collection) {
   HTTP.unpublish(baseUrl + '/record/' + collection.name);
 };
 
+var _existingMountPoints = {};
+
+/**
+ * @method defaultSelectorFunction
+ * @private
+ * @param {any} data Data returned by HTTP.method
+ * @returns { collection, file }
+ *
+ * This is the default selector function
+ */
+var defaultSelectorFunction = function(data) {
+  var self = this;
+  // Selector function
+  //
+  // This function will have to return the collection and the
+  // file. If file not found undefined is returned - if null is returned the
+  // search was not possible
+  var opts = _.extend({}, self.query || {}, self.params || {});
+
+  // Get the collection name from the url
+  var collectionName = opts.collectionName;
+
+  // Get the id from the url
+  var id = opts.id;
+
+  // Get the collection
+  var collection = FS._collections[collectionName];
+
+  // Get the file if possible else return null
+  var file = (id && collection)? collection.findOne({ _id: id }): null;
+
+  // Return the collection and the file
+  return {
+    collection: collection,
+    file: file
+  };
+};
 
 /*
-  @param
-  @param {function} selector_f is a function that simply returns the collection and file
-  @return {object} the collection and file if not found then undefined if search not possible then return null
+ * @method FS.HTTP.mount
+ * @public
+ * @param {array of string} mountPoints mount points to map rest functinality on
+ * @param {function} selector_f [selector] function returns `{ collection, file }` for mount points to work with
+ *
 */
 FS.HTTP.mount = function(mountPoints, selector_f) {
   // We take mount points as an array and we get a selector function
+  var selectorFunction = selector_f || defaultSelectorFunction;
 
   var accessPoint = {
     'post': function(data) {
       // Use the selector for finding the collection and file reference
-      var ref = selector_f.apply(this, [data]);
+      var ref = selectorFunction.apply(this, [data]);
 
       // We dont support post - this would be normal insert eg. of filerecord?
       throw new Meteor.Error(501, "Not implemented", "Post is not supported");
     },
     'put': function(data) {
       // Use the selector for finding the collection and file reference
-      var ref = selector_f.apply(this, [data]);
+      var ref = selectorFunction.apply(this, [data]);
 
       // Make sure we have a collection reference
       if (!ref.collection)
@@ -99,7 +139,7 @@ FS.HTTP.mount = function(mountPoints, selector_f) {
     },
     'get': function(data) {
       // Use the selector for finding the collection and file reference
-      var ref = selector_f.apply(this, [data]);
+      var ref = selectorFunction.apply(this, [data]);
 
       // Make sure we have a collection reference
       if (!ref.collection)
@@ -120,7 +160,7 @@ FS.HTTP.mount = function(mountPoints, selector_f) {
     },
     'delete': function(data) {
       // Use the selector for finding the collection and file reference
-      var ref = selector_f.apply(this, [data]);
+      var ref = selectorFunction.apply(this, [data]);
 
       // Make sure we have a collection reference
       if (!ref.collection)
@@ -143,6 +183,8 @@ FS.HTTP.mount = function(mountPoints, selector_f) {
   _.each(mountPoints, function(mountPoint) {
     // Couple mountpoint and accesspoint
     accessPoints[mountPoint] = accessPoint;
+    // Remember our mountpoints
+    _existingMountPoints[mountPoint] = mountPoint;
     // Add debug message
     FS.debug && console.log(mountPoint);
   });
@@ -152,39 +194,76 @@ FS.HTTP.mount = function(mountPoints, selector_f) {
 
 };
 
+/**
+ * @method FS.HTTP.unmount
+ * @public
+ * @param {string | array of string} [mountPoints] Optional, if not specified all mountpoints are unmounted
+ *
+ */
+FS.HTTP.unmount = function(mountPoints) {
+  // The mountPoints is optional, can be string or array if undefined then
+  // _existingMountPoints will be used
+  var unmountList;
+  // Container for the mount points to unmount
+  var unmountPoints = {};
 
+  if (typeof mountPoints === 'undefined') {
+    // Use existing mount points - unmount all
+    unmountList = _existingMountPoints;
+  } else if (mountPoints === ''+mountPoints) {
+    // Got a string
+    unmountList = [mountPoints];
+  } else if (mountPoints.length) {
+    // Got an array
+    unmountList = mountPoints;
+  }
+
+  // If we have a list to unmount
+  if (unmountList) {
+    // Iterate over each item
+    _.each(unmountList, function(mountPoint) {
+      // Check _existingMountPoints to make sure the mount point exists in our
+      // context / was created by the FS.HTTP.mount
+      if (_existingMountPoints[mountPoint]) {
+        // Mark as unmount
+        unmountPoints[mountPoint] = false;
+        // Release
+        delete _existingMountPoints[mountPoint];
+      }
+    });
+    FS.debug && console.log('FS.HTTP.unmount:');
+    FS.debug && console.log(unmountPoints);
+    // Complete unmount
+    HTTP.methods(unmountPoints);
+  }
+};
+
+// ### FS.Collection maps on HTTP pr. default on the following restpoints:
+// *
+//    baseUrl + '/files/:collectionName/:id/:filename',
+//    baseUrl + '/files/:collectionName/:id',
+//    baseUrl + '/files/:collectionName'
+//
+// Change/ replace the existing mount point by:
+// ```js
+//   // unmount all existing
+//   FS.HTTP.unmount();
+//   // Create new mount point
+//   FS.HTTP.mount([
+//    '/cfs/files/:collectionName/:id/:filename',
+//    '/cfs/files/:collectionName/:id',
+//    '/cfs/files/:collectionName'
+//  ]);
+//  ```
+//
 Meteor.startup(function () {
+
+  // Start up the basic mount points
   FS.HTTP.mount([
     baseUrl + '/files/:collectionName/:id/:filename',
     baseUrl + '/files/:collectionName/:id',
     baseUrl + '/files/:collectionName'
-  ], function(data) {
-    var self = this;
-    // Selector function
-    //
-    // This function will have to return the collection and the
-    // file. If file not found undefined is returned - if null is returned the
-    // search was not possible
-    var opts = _.extend({}, self.query || {}, self.params || {});
-
-    // Get the collection name from the url
-    var collectionName = opts.collectionName;
-
-    // Get the id from the url
-    var id = opts.id;
-
-    // Get the collection
-    var collection = FS._collections[collectionName];
-
-    // Get the file if possible else return null
-    var file = (id && collection)? collection.findOne({ _id: id }): null;
-
-    // Return the collection and the file
-    return {
-      collection: collection,
-      file: file
-    };
-  });
+  ]);
 
   // FS.debug && console.log("Registered HTTP method URLs:\n\n" + currentHTTPMethodNames.join('\n') + '\n');
 });
