49dca8d66765e5d54cf9af0e7a35c8963d3f0a89
Morten Henriksen <mh@gi2.dk>
2014-03-12 09:00:36 +0100

    deprecate code

--- storageAdapter.server.js
+++ storageAdapter.server.js
@@ -19,7 +19,8 @@ FS.StorageAdapter = function(name, options, api) {
     throw new Error('FS.StorageAdapter please define an api');
   }
 
-  _.each('get,put,del,typeName,createReadStream,createWriteStream'.split(','), function(name) {
+  // Deprecate put & get maybe refactor del into remove
+  _.each('del,typeName,createReadStream,createWriteStream'.split(','), function(name) {
     if (typeof api[name] === 'undefined') {
       throw new Error('FS.StorageAdapter please define an api.' + name + '');
     }
@@ -36,57 +37,6 @@ FS.StorageAdapter = function(name, options, api) {
   _.extend(this, options || {}, {
     name: name
   });
-
-  var foCheck = function(fsFile, type) {
-    if (!(fsFile instanceof FS.File)) {
-      throw new Error('Storage adapter "' + name + '" ' + type + ' requires fsFile');
-    }
-    if (!fsFile.data && (type === "insert" || type === "update")) {
-      throw new Error('Storage adapter "' + name + '" ' + type + ' requires fsFile with data');
-    }
-  };
-
-  function doPut(fsFile, overwrite, callback) {
-
-    // Prep file info to be returned
-    var savedFileInfo = {
-      name: fsFile.name,
-      type: fsFile.type,
-      size: fsFile.size
-    };
-
-    // Create callback for store `put` method
-    function putCallback(err, finalFileKey) {
-      if (err) {
-        callback(err, null);
-      } else if (!finalFileKey) {
-        callback(new Error("No file key"), null);
-      } else {
-
-        function finish(updatedAt) {
-          savedFileInfo.key = finalFileKey;
-          savedFileInfo.utime = updatedAt;
-          callback(err, err ? null : savedFileInfo);
-        }
-
-        // note the file key and updatedAt in the SA file record
-        if (typeof api.stats === "function") {
-          api.stats.call(self, finalFileKey, function(err, stats) {
-            if (err) {
-              callback(err, null);
-            } else {
-              finish(stats.mtime);
-            }
-          });
-        } else {
-          finish(new Date);
-        }
-      }
-    }
-
-    // Put the file to storage
-    api.put.call(self, fsFile, {overwrite: overwrite}, putCallback);
-  }
 
   // Create a nicer abstracted adapter interface
   self.adapter = {};
@@ -118,76 +68,6 @@ FS.StorageAdapter = function(name, options, api) {
     return FS.Utility.safeStream(api.createWriteStream.call(self, fileObj, options) );
   };
 
-  //internal
-  self._insertAsync = function(fsFile, callback) {
-    return doPut(fsFile, false, callback);
-  };
-
-  /**
-   * @method FS.StorageAdapter.prototype.insert
-   * @public
-   * @param {FS.File} fsFile The FS.File instance to be stored.
-   * @param {Object} [options] Options (currently unused)
-   * @param {Function} [callback] If not provided, will block and return file info.
-   * @deprecated Use streams api instead
-   *
-   * Attempts to insert a file into the store. If there is a temporary failure,
-   * returns (or passes to the second argument of the callback) `null`. If there
-   * is a permanant failure, returns or passes `false`. If the file is
-   * successfully stored, returns an object with file info that the
-   * FS.Collection can save.
-   */
-  self.insert = function(fsFile, options, callback) {
-    foCheck(fsFile, "insert");
-
-    if (!callback && typeof options === "function") {
-      callback = options;
-      options = {};
-    }
-
-    FS.debug && console.log("---SA INSERT callback: " + (typeof callback === 'function'));
-
-    if (callback) {
-      return self._insertAsync(fsFile, FS.Utility.safeCallback(callback));
-    } else {
-      return Meteor._wrapAsync(self._insertAsync)(fsFile);
-    }
-  };
-
-  //internal
-  self._updateAsync = function(fsFile, callback) {
-    return doPut(fsFile, true, callback);
-  };
-
-  /**
-   * @method FS.StorageAdapter.prototype.update
-   * @public
-   * @param {FS.File} fsFile The FS.File instance to be stored.
-   * @param {Object} [options] Options (currently unused)
-   * @param {Function} [callback] If not provided, will block and return file info.
-   * @deprecated Use streams api instead
-   *
-   * Attempts to update a file in the store. If there is a temporary failure,
-   * returns (or passes to the second argument of the callback) `null`. If there
-   * is a permanant failure, returns or passes `false`. If the file is
-   * successfully stored, returns an object with file info that the
-   * FS.Collection can save.
-   */
-  self.update = function(fsFile, options, callback) {
-    FS.debug && console.log("---SA UPDATE");
-    foCheck(fsFile, "update");
-
-    if (!callback && typeof options === "function") {
-      callback = options;
-      options = {};
-    }
-
-    if (callback) {
-      return self._updateAsync(fsFile, FS.Utility.safeCallback(callback));
-    } else {
-      return Meteor._wrapAsync(self._updateAsync)(fsFile);
-    }
-  };
 
   //internal
   self._removeAsync = function(fsFile, callback) {
@@ -207,7 +87,8 @@ FS.StorageAdapter = function(name, options, api) {
    */
   self.remove = function(fsFile, options, callback) {
     FS.debug && console.log("---SA REMOVE");
-    foCheck(fsFile, "remove");
+    if (!(fsFile instanceof FS.File))
+      throw new Error('Storage adapter "' + name + '" remove requires fsFile');
 
     if (!callback && typeof options === "function") {
       callback = options;
@@ -222,86 +103,228 @@ FS.StorageAdapter = function(name, options, api) {
     }
   };
 
-  //internal
-  self._getBufferAsync = function(fsFile, callback) {
-    // get the buffer
-    api.get.call(self, fsFile, callback);
-  };
 
-  /**
-   * @method FS.StorageAdapter.prototype.getBuffer
-   * @public
-   * @param {FS.File} fsFile The FS.File instance to be retrieved.
-   * @param {Object} [options] unused
-   * @param {Function} [callback] If not provided, will block and return the Buffer
-   * @deprecated Use streams api instead
-   *
-   * Returns the buffer for a file that has been saved in this store
-   */
-  self.getBuffer = function(fsFile, options, callback) {
-    FS.debug && console.log("---SA GET BUFFER");
-    foCheck(fsFile, "getBuffer");
+///////////////////////////////////////////////////////////////////////////
+// All below has deprecated
 
-    if (!callback && typeof options === "function") {
-      callback = options;
-      options = {};
-    }
+  // var foCheck = function(fsFile, type) {
+  //   throw new Error('deprecated');
+  //   if (!(fsFile instanceof FS.File)) {
+  //     throw new Error('Storage adapter "' + name + '" ' + type + ' requires fsFile');
+  //   }
+  //   if (!fsFile.data && (type === "insert" || type === "update")) {
+  //     throw new Error('Storage adapter "' + name + '" ' + type + ' requires fsFile with data');
+  //   }
+  // };
 
-    if (callback) {
-      return self._getBufferAsync(fsFile, FS.Utility.safeCallback(callback));
-    } else {
-      return Meteor._wrapAsync(self._getBufferAsync)(fsFile);
-    }
-  };
 
-  if (typeof api.getBytes === 'function') {
-    //internal
-    self._getBytesAsync = function(fsFile, start, end, callback) {
-      var copyInfo = fsFile.getCopyInfo(self.name);
-      if (!copyInfo) {
-        callback(new Error("No file info found for the " + self.name + " store. Can't getBytes."), false);
-        return;
-      }
+  // function doPut(fsFile, overwrite, callback) {
+  //   throw new Error('deprecated');
+  //   // Prep file info to be returned
+  //   var savedFileInfo = {
+  //     name: fsFile.name,
+  //     type: fsFile.type,
+  //     size: fsFile.size
+  //   };
 
-      if (copyInfo.size) {
-        end = Math.min(end, copyInfo.size);
-      }
+  //   // Create callback for store `put` method
+  //   function putCallback(err, finalFileKey) {
+  //     if (err) {
+  //       callback(err, null);
+  //     } else if (!finalFileKey) {
+  //       callback(new Error("No file key"), null);
+  //     } else {
 
-      // get the buffer
-      api.getBytes.call(self, fsFile, start, end, callback);
-    };
+  //       function finish(updatedAt) {
+  //         savedFileInfo.key = finalFileKey;
+  //         savedFileInfo.utime = updatedAt;
+  //         callback(err, err ? null : savedFileInfo);
+  //       }
 
-   /**
-    * @method FS.StorageAdapter.prototype.getBytes
-    * @public
-    * @param {FS.File} fsFile The FS.File instance to be retrieved.
-    * @param {Number} start - The position of the first byte to return
-    * @param {Number} end - The position of the last byte to return, plus one
-    * @param {Object} [options] unused
-    * @param {Function} [callback] If not provided, will block and return the Buffer
-    * @deprecated Use streams api instead
-    *
-    * Returns the buffer for one chunk of a file that has been saved in this store
-    */
-    self.getBytes = function(fsFile, start, end, options, callback) {
-      FS.debug && console.log("---SA GET BYTES");
-      foCheck(fsFile, "getBytes");
+  //       // note the file key and updatedAt in the SA file record
+  //       if (typeof api.stats === "function") {
+  //         api.stats.call(self, finalFileKey, function(err, stats) {
+  //           if (err) {
+  //             callback(err, null);
+  //           } else {
+  //             finish(stats.mtime);
+  //           }
+  //         });
+  //       } else {
+  //         finish(new Date);
+  //       }
+  //     }
+  //   }
 
-      if (!callback && typeof options === "function") {
-        callback = options;
-        options = {};
-      }
+  //   // Put the file to storage
+  //   api.put.call(self, fsFile, {overwrite: overwrite}, putCallback);
+  // }
 
-      if (callback) {
-        return self._getBytesAsync(fsFile, start, end, FS.Utility.safeCallback(callback));
-      } else {
-        return Meteor._wrapAsync(self._getBytesAsync)(fsFile, start, end);
-      }
-    };
-  }
+  // //internal
+  // self._insertAsync = function(fsFile, callback) {
+  //   throw new Error('deprecated');
+  //   return doPut(fsFile, false, callback);
+  // };
 
-  if (typeof api.init === 'function') {
-    Meteor._wrapAsync(api.init.bind(self))();
-  }
+  // /**
+  //  * @method FS.StorageAdapter.prototype.insert
+  //  * @public
+  //  * @param {FS.File} fsFile The FS.File instance to be stored.
+  //  * @param {Object} [options] Options (currently unused)
+  //  * @param {Function} [callback] If not provided, will block and return file info.
+  //  * @deprecated Use streams api instead
+  //  *
+  //  * Attempts to insert a file into the store. If there is a temporary failure,
+  //  * returns (or passes to the second argument of the callback) `null`. If there
+  //  * is a permanant failure, returns or passes `false`. If the file is
+  //  * successfully stored, returns an object with file info that the
+  //  * FS.Collection can save.
+  //  */
+  // self.insert = function(fsFile, options, callback) {
+  //   throw new Error('deprecated');
+
+  //   foCheck(fsFile, "insert");
+
+  //   if (!callback && typeof options === "function") {
+  //     callback = options;
+  //     options = {};
+  //   }
+
+  //   FS.debug && console.log("---SA INSERT callback: " + (typeof callback === 'function'));
+
+  //   if (callback) {
+  //     return self._insertAsync(fsFile, FS.Utility.safeCallback(callback));
+  //   } else {
+  //     return Meteor._wrapAsync(self._insertAsync)(fsFile);
+  //   }
+  // };
+
+  // //internal
+  // self._updateAsync = function(fsFile, callback) {
+  //   throw new Error('deprecated');
+
+  //   return doPut(fsFile, true, callback);
+  // };
+
+  // *
+  //  * @method FS.StorageAdapter.prototype.update
+  //  * @public
+  //  * @param {FS.File} fsFile The FS.File instance to be stored.
+  //  * @param {Object} [options] Options (currently unused)
+  //  * @param {Function} [callback] If not provided, will block and return file info.
+  //  * @deprecated Use streams api instead
+  //  *
+  //  * Attempts to update a file in the store. If there is a temporary failure,
+  //  * returns (or passes to the second argument of the callback) `null`. If there
+  //  * is a permanant failure, returns or passes `false`. If the file is
+  //  * successfully stored, returns an object with file info that the
+  //  * FS.Collection can save.
+
+  // self.update = function(fsFile, options, callback) {
+  //   throw new Error('deprecated');
+
+  //   FS.debug && console.log("---SA UPDATE");
+  //   foCheck(fsFile, "update");
+
+  //   if (!callback && typeof options === "function") {
+  //     callback = options;
+  //     options = {};
+  //   }
+
+  //   if (callback) {
+  //     return self._updateAsync(fsFile, FS.Utility.safeCallback(callback));
+  //   } else {
+  //     return Meteor._wrapAsync(self._updateAsync)(fsFile);
+  //   }
+  // };
+
+  // //internal
+  // self._getBufferAsync = function(fsFile, callback) {
+  //   throw new Error('deprecated');
+
+  //   // get the buffer
+  //   api.get.call(self, fsFile, callback);
+  // };
+
+  // /**
+  //  * @method FS.StorageAdapter.prototype.getBuffer
+  //  * @public
+  //  * @param {FS.File} fsFile The FS.File instance to be retrieved.
+  //  * @param {Object} [options] unused
+  //  * @param {Function} [callback] If not provided, will block and return the Buffer
+  //  * @deprecated Use streams api instead
+  //  *
+  //  * Returns the buffer for a file that has been saved in this store
+  //  */
+  // self.getBuffer = function(fsFile, options, callback) {
+  //   throw new Error('deprecated');
+
+  //   FS.debug && console.log("---SA GET BUFFER");
+  //   foCheck(fsFile, "getBuffer");
+
+  //   if (!callback && typeof options === "function") {
+  //     callback = options;
+  //     options = {};
+  //   }
+
+  //   if (callback) {
+  //     return self._getBufferAsync(fsFile, FS.Utility.safeCallback(callback));
+  //   } else {
+  //     return Meteor._wrapAsync(self._getBufferAsync)(fsFile);
+  //   }
+  // };
+
+  // if (typeof api.getBytes === 'function') {
+  //   //internal
+  //   self._getBytesAsync = function(fsFile, start, end, callback) {
+  //     throw new Error('deprecated');
+  //     var copyInfo = fsFile.getCopyInfo(self.name);
+  //     if (!copyInfo) {
+  //       callback(new Error("No file info found for the " + self.name + " store. Can't getBytes."), false);
+  //       return;
+  //     }
+
+  //     if (copyInfo.size) {
+  //       end = Math.min(end, copyInfo.size);
+  //     }
+
+  //     // get the buffer
+  //     api.getBytes.call(self, fsFile, start, end, callback);
+  //   };
+
+  //  /**
+  //   * @method FS.StorageAdapter.prototype.getBytes
+  //   * @public
+  //   * @param {FS.File} fsFile The FS.File instance to be retrieved.
+  //   * @param {Number} start - The position of the first byte to return
+  //   * @param {Number} end - The position of the last byte to return, plus one
+  //   * @param {Object} [options] unused
+  //   * @param {Function} [callback] If not provided, will block and return the Buffer
+  //   * @deprecated Use streams api instead
+  //   *
+  //   * Returns the buffer for one chunk of a file that has been saved in this store
+  //   */
+  //   self.getBytes = function(fsFile, start, end, options, callback) {
+  //     throw new Error('deprecated');
+
+  //     FS.debug && console.log("---SA GET BYTES");
+  //     foCheck(fsFile, "getBytes");
+
+  //     if (!callback && typeof options === "function") {
+  //       callback = options;
+  //       options = {};
+  //     }
+
+  //     if (callback) {
+  //       return self._getBytesAsync(fsFile, start, end, FS.Utility.safeCallback(callback));
+  //     } else {
+  //       return Meteor._wrapAsync(self._getBytesAsync)(fsFile, start, end);
+  //     }
+  //   };
+  // }
+
+  // if (typeof api.init === 'function') {
+  //   Meteor._wrapAsync(api.init.bind(self))();
+  // }
 
 };
