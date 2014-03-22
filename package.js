0bd3b68283bcc4141c711c7519378f625c000a4b
Morten Henriksen <mh@gi2.dk>
2014-03-22 17:45:55 +0100

    Testing 3 different writeStream / upload patterns for s3
    So this is the ground work for picking the best way of creating a write
    stream for the s3 SA
    1. the `s3.indirect.streaming.js` loads the write stream into temp
    storage to get the content length then uses the putObject streaming api
    for uploading the image
    2. `s3.upload.stream.js` this loads the data from the write stream into
    a buffer and slices data chunks out and sends it the the s3 server
    3. `s3.upload.stream2.js` this one is streaming the writes and
    buffering up data to min. 5mb chunks then uploads the chunk. This only
    buffers a max of one chunk (min 5mb)
    
    I’m not sure which is best - 1 or 3 because 2 buffers the whole file in
    memory and got a lot of dependencies.
    1. because its using the filesystem stream and connects it directly to
    the s3.putObject stream api - and everything is handled for us
    3. is perhaps the best because we skip the filesystem temp file -
    otherwise basically the same stuff.
    
    It could be fun to benchmark and test how much memory is actually used
    for these 3 methods.
    
    cc @aldeed @vsivsi

--- package.js
+++ package.js
@@ -4,13 +4,21 @@ Package.describe({
 
 Npm.depends({
   'aws-sdk': "2.0.0-rc9",
-  'temp': '0.7.0'
+  // 'temp': '0.7.0', // used by the s3.indirect.streaming.js
+  // 'through2': '0.4.1', // used by the s3.upload.stream.js
+  // 'backoff': '2.3.0', // used by the s3.upload.stream.js
+  // 'bl': '0.7.0' // used by the s3.upload.stream.js
 });
 
 Package.on_use(function(api) {
   api.use(['cfs-base-package', 'cfs-storage-adapter']);
   api.use(['underscore']);
-  api.add_files(['s3.server.js', 's3.indirect.streaming.js'], 'server');
+  api.add_files([
+    's3.server.js',
+    // 's3.indirect.streaming.js',
+    // 's3.upload.stream.js',
+    's3.upload.stream2.js',
+    ], 'server');
   api.add_files('s3.client.js', 'client');
 });
 
