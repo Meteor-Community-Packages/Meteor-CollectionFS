0831c2a7afa4faafc37bb31c18d466a904b257eb
Morten Henriksen <mh@gi2.dk>
2014-03-29 18:02:36 +0100

    Remove underscore deps

--- gridfs.client.js
+++ gridfs.client.js
@@ -6,7 +6,7 @@
  * @param {Function} [options.beforeSave] - Function to run before saving a file from the client. The context of the function will be the `FS.File` instance we're saving. The function may alter its properties.
  * @param {Number} [options.maxTries=5] - Max times to attempt saving a file
  * @returns {undefined}
- * 
+ *
  * Creates a GridFS store instance on the client, which is just a shell object
  * storing some info.
  */
@@ -14,6 +14,6 @@ FS.Store.GridFS = function(name, options) {
   var self = this;
   if (!(self instanceof FS.Store.GridFS))
     throw new Error('FS.Store.GridFS missing keyword "new"');
-  
-  _.extend(this, { name: name, sync: false, maxTries: 5 }, options || {});
+
+  FS.Utility.extend(this, { name: name, sync: false, maxTries: 5 }, options || {});
 };
