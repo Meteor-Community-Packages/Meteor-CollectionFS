a27c80d8fdd5317c20bff4cc19f52c8ddb097e46
Morten Henriksen <mh@gi2.dk>
2014-04-02 09:33:55 +0200

    use the gfs api for removing the file
    @aldeed I haven’t confirmed that this works - eg. looking into the db

--- gridfs.server.js
+++ gridfs.server.js
@@ -96,10 +96,18 @@ FS.Store.GridFS = function(name, options) {
 
     },
     remove: function(fileKey, callback) {
-      mongodb.GridStore.unlink(self.db, fileKey, { root: gridfsName }, function (err) {
-        if (err) { return callback(err); }
-        callback(null, true);
-      });
+      // Init GridFS
+      var gfs = new Grid(self.db, mongodb);
+
+      try {
+        gfs.remove({ _id: fileKey }, callback);
+      } catch(err) {
+        callback(err);
+      }
+      // mongodb.GridStore.unlink(self.db, fileKey, { root: gridfsName }, function (err) {
+      //   if (err) { return callback(err); }
+      //   callback(null, true);
+      // });
     },
 
     // Not implemented
