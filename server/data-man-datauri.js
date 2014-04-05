b094e64e846035ee9afb8a718d66715be2e5f72b
Eric Dobbertin <eric@dairystatedesigns.com>
2014-04-05 13:51:09 -0500

    DataMan.DataURI now passes off handling to DataMan.Buffer

--- server/data-man-datauri.js
+++ server/data-man-datauri.js
@@ -1,77 +1,8 @@
-var sbs = Npm.require('simple-bufferstream');
-
 DataMan.DataURI = function DataManDataURI(dataUri) {
   var self = this;
-  self.dataUri = dataUri;
-  self._type = dataUri.slice(5, dataUri.indexOf(';'));
+  var pieces = dataUri.match(/^data:(.*);base64,(.*)$/);
+  var buffer = new Buffer(pieces[2], 'base64');
+  return new DataMan.Buffer(buffer, pieces[1]);
 };
 
-/**
- * @method DataMan.DataURI.prototype.getBuffer
- * @private
- * @param {function} callback callback(err, buffer)
- * @returns {Buffer|undefined}
- *
- * Passes a Buffer representing the data to a callback.
- */
-DataMan.DataURI.prototype.getBuffer = function dataManDataURIGetBuffer(callback) {
-  var self = this;
-  var data = self.dataUri.substr(self.dataUri.indexOf('base64') + 7);
-  callback(null, new Buffer(data, 'base64'));
-};
-
-/**
- * @method DataMan.DataURI.prototype.getDataUri
- * @private
- * @param {function} callback callback(err, dataUri)
- *
- * Passes a data URI representing the data to a callback.
- */
-DataMan.DataURI.prototype.getDataUri = function dataManDataURIGetDataUri(callback) {
-  callback(null, this.dataUri);
-};
-
-/**
- * @method DataMan.DataURI.prototype.createReadStream
- * @private
- *
- * Returns a read stream for the data.
- */
-DataMan.DataURI.prototype.createReadStream = function dataManDataURICreateReadStream() {
-  return sbs(Meteor._wrapAsync(this.getBuffer)());
-};
-
-/**
- * @method DataMan.DataURI.prototype.size
- * @param {function} callback callback(err, size)
- * @private
- *
- * Passes the size in bytes of the data to a callback.
- */
-DataMan.DataURI.prototype.size = function dataManDataURISize(callback) {
-  var self = this;
-
-  if (typeof self._size === "number") {
-    callback(null, self._size);
-    return;
-  }
-
-  self.getBuffer(function (error, buffer) {
-    if (error) {
-      callback(error);
-    } else {
-      self._size = buffer.length;
-      callback(null, self._size);
-    }
-  });
-};
-
-/**
- * @method DataMan.DataURI.prototype.type
- * @private
- *
- * Returns the type of the data.
- */
-DataMan.DataURI.prototype.type = function dataManDataURIType() {
-  return this._type;
-};
+DataMan.DataURI.prototype = DataMan.Buffer.prototype;
