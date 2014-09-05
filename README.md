89f2a82d50d530dfce7ae2782a1a25f038df3423
Morten Henriksen <mh@gi2.dk>
2014-09-05 09:13:49 +0200

    0.9.1 support

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-cfs-collection
+cfs:collection
 =========================
 
 This is a Meteor package used by
 [CollectionFS](https://github.com/CollectionFS/Meteor-CollectionFS).
 
 You don't need to manually add this package to your app. It is added when you
-add the `collectionFS` package.
+add the `cfs:standard-packages` package.
--- package.js
+++ package.js
@@ -1,24 +1,27 @@
 Package.describe({
+  name: 'cfs:collection',
   version: '0.0.0',
   summary: 'CollectionFS, FS.Collection object'
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom('METEOR@0.9.1');
+
   api.use([
     // CFS
-    'cfs-base-package',
-    'cfs-tempstore',
+    'cfs:base-package@0.0.0',
+    'cfs:tempstore@0.0.0',
     // Core
     'deps',
     'check',
     'livedata',
     'mongo-livedata',
     // Other
-    'emitter'
+    'raix:eventemitter@0.0.1'
   ]);
 
   // Weak dependencies for uploaders
-  api.use(['cfs-upload-http', 'cfs-upload-ddp'], { weak: true });
+  api.use(['cfs:upload-http@0.0.0', 'cfs:upload-ddp@0.0.0'], { weak: true });
 
   api.add_files([
     'common.js',
@@ -31,13 +34,13 @@ Package.on_use(function(api) {
   ], 'server');
 });
 
-Package.on_test(function (api) {
-  // api.use('collectionfs');
-  // api.use('test-helpers', 'server');
-  // api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
-  //          'random', 'deps']);
-  api.use(['collectionfs', 'cfs-gridfs', 'tinytest', 'underscore', 'test-helpers']);
+// Package.on_test(function (api) {
+//   // api.use('collectionfs');
+//   // api.use('test-helpers', 'server');
+//   // api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
+//   //          'random', 'deps']);
+//   api.use(['collectionfs', 'cfs:gridfs@0.0.0', 'tinytest', 'underscore', 'test-helpers']);
 
-  api.add_files('tests/server-tests.js', 'server');
-  api.add_files('tests/client-tests.js', 'client');
-});
+//   api.add_files('tests/server-tests.js', 'server');
+//   api.add_files('tests/client-tests.js', 'client');
+// });
