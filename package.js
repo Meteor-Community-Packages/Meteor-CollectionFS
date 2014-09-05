7e3a7d0de23f6da1e33787fac533dc3310340afc
Morten Henriksen <mh@gi2.dk>
2014-09-05 09:15:40 +0200

    0.9.1 support

--- package.js
+++ package.js
@@ -1,4 +1,5 @@
  Package.describe({
+  name: 'cfs:tempstore',
   version: '0.0.0',
   summary: 'CollectionFS, temporary storage'
 });
@@ -8,21 +9,23 @@ Npm.depends({
 });
 
 Package.on_use(function(api) {
-  api.use(['cfs-base-package', 'cfs-file']);
+  api.versionsFrom('METEOR@0.9.1');
 
-  api.use('cfs-filesystem', { weak: true });
-  api.use('cfs-gridfs', { weak: true });
+  api.use(['cfs:base-package@0.0.0', 'cfs:file@0.0.0']);
+
+  api.use('cfs:filesystem@0.0.0', { weak: true });
+  api.use('cfs:gridfs@0.0.0', { weak: true });
 
   api.add_files([
     'tempStore.js'
   ], 'server');
 });
 
-Package.on_test(function (api) {
-  api.use('collectionfs');
-  api.use('test-helpers', 'server');
-  api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
-           'random', 'deps']);
+// Package.on_test(function (api) {
+//   api.use('collectionfs');
+//   api.use('test-helpers', 'server');
+//   api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
+//            'random', 'deps']);
 
-  api.add_files('tests/server-tests.js', 'server');
-});
+//   api.add_files('tests/server-tests.js', 'server');
+// });
