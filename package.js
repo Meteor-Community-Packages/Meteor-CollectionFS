087c853985b1775b2b957a66d33a1b08c70465c2
Morten Henriksen <mh@gi2.dk>
2014-12-17 14:05:50 +0100

    mbr update versions and fix warnings

--- package.js
+++ package.js
@@ -1,33 +1,34 @@
 Package.describe({
+  git: 'https://github.com/CollectionFS/Meteor-CollectionFS.git',
   name: 'cfs:standard-packages',
   version: '0.0.2',
   summary: 'Filesystem for Meteor, collectionFS'
 });
 
-Package.on_use(function(api) {
-  api.versionsFrom('METEOR@0.9.1');
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
   
   // Rig the collectionFS package v2
   api.imply([
     // Base util rigs the basis for the FS scope and some general helper mehtods
-    'cfs:base-package@0.0.0',
+    'cfs:base-package@0.0.26',
     // Want to make use of the file object and its api, yes!
     'cfs:file@0.0.0',
     // Add the FS.Collection to keep track of everything
-    'cfs:collection@0.0.0',
+    'cfs:collection@0.0.1',
     // Support filters for easy rules about what may be inserted
     'cfs:collection-filters@0.0.0',
     // Add the option to have ddp and http access point
-    'cfs:access-point@0.0.0',
+    'cfs:access-point@0.1.40',
     // We might also want to have the server create copies of our files?
     'cfs:worker@0.0.0',
     // By default we want to support uploads over HTTP
-    'cfs:upload-http@0.0.2',
+    'cfs:upload-http@0.0.16',
   ]);
 
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('cfs:standard-packages');
   api.use('test-helpers', 'server');
   api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
