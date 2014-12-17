376fba1c7075f1e9ba338261150128a907e99a50
Morten Henriksen <mh@gi2.dk>
2014-12-17 16:42:46 +0100

    bump versions

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.use([
     'cfs:base-package@0.0.26',
     'cfs:tempstore@0.1.0',
-    'cfs:file@0.0.14',
+    'cfs:file@0.0.15',
     'cfs:access-point@0.1.40',
     'cfs:power-queue@0.9.10',
     'cfs:reactive-list@0.0.8'
