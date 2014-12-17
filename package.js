3606ab9bb713ccd9b238ab3eb1af244b30e97144
Morten Henriksen <mh@gi2.dk>
2014-12-17 21:04:50 +0100

    bump versions

--- package.js
+++ package.js
@@ -17,7 +17,7 @@ Package.onUse(function(api) {
     // Add the FS.Collection to keep track of everything
     'cfs:collection@0.5.2',
     // Support filters for easy rules about what may be inserted
-    'cfs:collection-filters@0.2.1',
+    'cfs:collection-filters@0.2.2',
     // Add the option to have ddp and http access point
     'cfs:access-point@0.1.42',
     // We might also want to have the server create copies of our files?
