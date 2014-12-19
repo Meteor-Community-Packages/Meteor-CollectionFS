265b920ecd05763c94f2c6ee8df614c6a1c5fb12
Morten Henriksen <mh@gi2.dk>
2014-12-19 21:26:31 +0100

    bump deps

--- package.js
+++ package.js
@@ -23,7 +23,7 @@ Package.onUse(function(api) {
     // We might also want to have the server create copies of our files?
     'cfs:worker@0.1.2',
     // By default we want to support uploads over HTTP
-    'cfs:upload-http@0.0.18',
+    'cfs:upload-http@0.0.19',
   ]);
 
 });
