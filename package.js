a5ceecf53052b54cc9377e346a4b13922ece6f90
Morten Henriksen <mh@gi2.dk>
2014-12-20 00:28:42 +0100

    bump deps

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   git: 'https://github.com/CollectionFS/Meteor-cfs-collection-filters.git',
   name: 'cfs:collection-filters',
-  version: '0.2.2',
+  version: '0.2.3',
   summary: 'CollectionFS, adds FS.Collection filters'
 });
 
