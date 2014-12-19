191a91d9d83c3e1e10d8c62b594d5ec63a7ad8a2
Morten Henriksen <mh@gi2.dk>
2014-12-19 21:18:02 +0100

    update deps

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.use(['cfs:base-package@0.0.27', 'cfs:file@0.1.14']);
+  api.use(['cfs:base-package@0.0.27', 'cfs:file@0.1.15']);
 
   api.use('cfs:filesystem@0.1.1', { weak: true });
   api.use('cfs:gridfs@0.0.27', { weak: true });
