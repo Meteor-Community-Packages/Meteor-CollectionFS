65c2338cc9854e416f5189f65547b08801d0c2ad
Morten Henriksen <mh@gi2.dk>
2014-12-20 00:26:18 +0100

    bump deps

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.use(['cfs:base-package@0.0.27', 'cfs:collection@0.5.2']);
+  api.use(['cfs:base-package@0.0.27', 'cfs:collection@0.5.3']);
 
   api.addFiles([
     'filters.js'
