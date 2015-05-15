f178a123d437e7ef08439de2985a69fc6383b39a
Bryan te Beek <bryantebeek@gmail.com>
2015-05-15 14:40:47 +0200

    Use the correct variable in setData

--- packages/file/fsFile-common.js
+++ packages/file/fsFile-common.js
@@ -88,7 +88,7 @@ FS.File.prototype.attachData = function fsFileAttachData(data, options, callback
             throw new Error('FS.File.attachData got a URL for which it could not determine the MIME type and none was provided using options.type');
           }
           FS.Utility.extend(self, {original: result});
-          setData(result.type);
+          setData(type);
         }
       });
     }
